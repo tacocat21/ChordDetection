31f33a50cc637de33965f00795cea5e65f36622c
Taccio Yamamoto <tyamamo2@illinois.edu>
2017-12-07 16:21:01 -0600

    added template w/ sevenths

--- src/models/template.py
+++ src/models/template.py
@@ -1,11 +1,13 @@
 import util
 import numpy as np
+import numpy.linalg as la
 import beatles_annotated_chroma
 
 class Template:
     def __init__(self):
         self.weights = [create_templates(i) for i in range(util.CHROMAGRAM_SIZE)]
-        self.weights.append([np.zeros(util.CHROMAGRAM_SIZE)])
+        # self.weights.append([np.zeros(util.CHROMAGRAM_SIZE)])
+        self.weights.append([np.array([0.1]*util.CHROMAGRAM_SIZE)])
 
     def predict_frame(self, frame):
         res = []
@@ -14,9 +16,9 @@ class Template:
             max_val = -1
             for w in weight:
                 calc = np.dot(frame, w)
-                max_val = max(calc, max_val)
+                max_val = min(calc, max_val)
             res.append(max_val)
-        return int(np.argmax(res))
+        return int(np.argmin(res))
 
     def predict(self, song):
         # res = []
@@ -24,16 +26,26 @@ class Template:
 
 
 def distance(v1, v2):
-    return np.dot(v1, v2)
+    return 1-np.dot(v1/la.norm(v1), v2/la.norm(v2))
+    # return la.norm(v1-v2, 2)
 
 def create_templates(idx):
     res = []
-    jumps = [[3,3], [3,4], [4,3], [4,4]]
-    for i, j in jumps:
+    # jumps = [[3,3], [3,4], [4,3], [4,4]]
+    jumps = [[3, 3], [3, 4], [4, 3], [4, 4],
+             [3, 3, 3], [3, 3, 4],  # diminished, half dim
+             [3, 4, 3], [3, 4, 4],  # minor, minormajor
+             [4, 3, 3], [4, 3, 4],  # dominant, major
+             [4, 4, 3],  # augmented
+             [2, 5], [5, 2]  # sus2, sus4
+             ]
+    for jump in jumps:
         z = np.zeros(util.CHROMAGRAM_SIZE)
+        curr = idx
         z[idx] = 1
-        z[(idx + i)% util.CHROMAGRAM_SIZE] = 1
-        z[(idx + i + j) % util.CHROMAGRAM_SIZE] = 1
+        for i in jump:
+            curr+= i
+            z[curr% util.CHROMAGRAM_SIZE] = 1
         res.append(z)
     return res
 
